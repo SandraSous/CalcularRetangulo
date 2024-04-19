a98accc05191b3f8c80fc81dfa03a8f96b7fbe8c
SandraSous <sandra.sousarosa1985@gmail.com>
2024-04-18 21:00:46 -0300

    Adicionar arquivos de projeto.

---
+++ CalcularRetangulo/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Globalization;
+
+
+namespace curso { 
+
+    class program { 
+    
+        static void Main(string[] args) {
+
+            double b, a, area, perimetro, diagonal;
+
+             b = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+             a = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            area = b * a;
+            perimetro = 2 * (b + a);
+            diagonal = Math.Sqrt(Math.Pow(b, 2.0) + Math.Pow(a, 2.0));
+            Console.WriteLine("Area = " + area.ToString("F4", CultureInfo.InvariantCulture));
+            Console.WriteLine("Perimetro = " + perimetro.ToString("F4", CultureInfo.InvariantCulture));
+            Console.WriteLine("Diagonal = " + diagonal.ToString("F4", CultureInfo.InvariantCulture));
+
+            Console.ReadLine();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
+
+
+
